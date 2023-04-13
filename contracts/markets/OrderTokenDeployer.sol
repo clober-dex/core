64354dc48152b872ec84dc5ddc8382ef40e120ac
graykode <nlkey2022@gmail.com>
2023-04-13 15:26:03 +0900

    fix: deploy function in `OrderTokenDeployer`

--- contracts/markets/OrderTokenDeployer.sol
+++ contracts/markets/OrderTokenDeployer.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/CloberOrderTokenDeployer.sol";
+import "../OrderNFT.sol";
 
 contract OrderTokenDeployer is CloberOrderTokenDeployer {
     address private immutable _factory;
@@ -14,5 +15,10 @@ contract OrderTokenDeployer is CloberOrderTokenDeployer {
         _canceler = canceler_;
     }
 
-    function deploy(bytes32 salt) external returns (address) {}
+    function deploy(bytes32 salt) external returns (address) {
+        if (msg.sender != _factory) {
+            revert Errors.CloberError(Errors.ACCESS);
+        }
+        return address(new OrderNFT{salt: salt}(_factory, _canceler));
+    }
 }
