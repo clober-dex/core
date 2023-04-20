b5530859a7c603d3236442754d677a68e9c2e8fa
JChoy <jchoyeclipse@gmail.com>
2023-04-20 16:54:00 +0900

    feat(task): add get-code util task

--- task/utils/network.ts
+++ task/utils/network.ts
@@ -6,3 +6,10 @@ task('utils:get-block', 'Print latest block')
     const block = await hre.ethers.provider.getBlock(taskArgs.where)
     console.log(block)
   })
+
+task('utils:get-code', 'Get code of a contract')
+  .addParam('address', 'target address')
+  .setAction(async (taskArgs, hre) => {
+    const code = await hre.ethers.provider.getCode(taskArgs.address)
+    console.log(code)
+  })
